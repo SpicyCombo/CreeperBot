886e2cb39685ba313c4358ab376e4275c6d11ff2
SpicyCombo <63688135+SpicyCombo@users.noreply.github.com>
2021-01-17 12:31:42 -0800

    more advanced shit

--- index.js
+++ index.js
@@ -19,83 +19,97 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 Licensed under the "MIT" license. See https://en.wikipedia.org/wiki/MIT_License
 */
 
-const Discord = require("discord.js");
-const client = new Discord.Client();
+// Before the bot starts, let the bot itself to see if the machine's NodeJS version is 12.0.0 or above. (From AnIdiotsGuide's GuideBot Github repository)
+if (Number(process.version.slice(1).split(".")[0]) < 12) throw new Error("Node 12.0.0 or higher is required. Update Node on your system.");
 
+// Discord.js is required for this bot to work.
+const Discord = require("discord.js");
+// Configurations are needed for some features to work. And I am making this now customization for you ;p
+const config = require("./config.js");
+// Core comtumizations, don't edit the core_config.js file if you still want the bot to stay cool :D
+const coreconfig = require("./core_config.js");
+
+// Now this is your client. Some people call it bot, some people call it clinet. You can call it whatever you want it to be, but I am going with client.
+const client = new Discord.Client({
+  ws: {
+    intents: config.intents
+  }
+});
+
+// Now, this is the place where the bot will send a message in console saying it's starting.
 client.once('ready', () => {
-	console.log("Creeper bot has started");
+	console.log(config.logstart); // Look in config.js if you would like to make changes to the message
 });
  
+// Alright, the bot is online. But, we want it to have some cool custom status (Bot presence) So lets make that happen
 client.on("ready", () => {
   client.user.setPresence({
       game: { 
-          name: 'c!help | Trying to blow up',
-          type: 'PLAYING'
+          name: 'c!help | Trying to blow up', // This is the status of the bot
+          type: 'PLAYING' // Type of that status
       },
-      status: 'idle'
+      status: 'idle' // Status status
   })
 });
 
-// Embed fields.
+const init = async () => {
+  // Load commands from the folder "core_commands". I named it that since it's core commands. Obvious... Right??
+  const cmdFiles = await readdir("./core_commands/");
+  client.logger.log(`Loading a total of ${cmdFiles.length} commands.`);
+  cmdFiles.forEach(f => {
+    if (!f.endsWith(".js")) return;
+    const response = client.loadCommand(f);
+    if (response) console.log(response);
+  });
+}
+
+/*
+const evtFiles = await readdir("./events/");
+client.logger.log(`Loading a total of ${evtFiles.length} events.`);
+evtFiles.forEach(file => {
+  const eventName = file.split(".")[0];
+  client.logger.log(`Loading Event: ${eventName}`);
+  const event = require(`./events/${file}`);
+  client.on(eventName, event.bind(null, client));
+});
+*/
+
+// Embeds.
 const lyricsEmbed = new Discord.MessageEmbed()
 	.setColor('#2fff00')
-	.setTitle('Creeper bot - Lyrics')
-	.setAuthor('Creeper bot', 'https://i.imgur.com/4AiJcoJ.png')
+	.setTitle(coreconfig.botname + ' - Lyrics')
+	.setAuthor(coreconfig.botname, 'https://i.imgur.com/4AiJcoJ.png')
 	.setDescription('These are the lyrics you can send to me for me to respond to!\n```Creeper\nAww man\nSo we back in the mine\nGot our pickaxe swinging from\nSide to side\nSide side to side\nThis task was a crueling one\nHope to find some diamonds tonight night nigh\nDiamonds tonight\n** SpicyCombo#1665 should add more of these..```')
 	.setTimestamp()
-  .setFooter('Creeper bot v1.0 by SpicyCombo#1665, made officially for Mikecraft Discord');
-  
-const helpEmbed = new Discord.MessageEmbed()
-	.setColor('#2fff00')
-	.setTitle('Creeper bot - Help')
-	.setAuthor('Creeper bot', 'https://i.imgur.com/4AiJcoJ.png')
-	.setDescription('**My prefix is** `c!`, the commands are:\n\n`help` - Displays this help message\n`ping` - Gets the latency of the bot.\n`restart` - Restarts the bot.')
-	.setTimestamp()
-	.setFooter('Creeper bot v1.0 by SpicyCombo#1665, made officially for Mikecraft Discord');
+  .setFooter(coreconfig.embedfooter);
 
 const shutdownEmbed = new Discord.MessageEmbed()
 	.setColor('#2fff00')
-	.setAuthor('Creeper bot', 'https://i.imgur.com/4AiJcoJ.png')
+	.setAuthor(coreconfig.botname, 'https://i.imgur.com/4AiJcoJ.png')
 	.setDescription('**Shutting down the bot, please check the console for more details.**')
 	.setTimestamp()
-	.setFooter('Creeper bot v1.0 by SpicyCombo#1665, made officially for Mikecraft Discord');
+	.setFooter(coreconfig.embedfooter);
 
 const pinglEmbed = new Discord.MessageEmbed()
 	.setColor('#2fff00')
-	.setTitle('Creeper bot - Ping')
-	.setAuthor('Creeper bot', 'https://i.imgur.com/4AiJcoJ.png')
+	.setTitle(coreconfig.botname + ' - Ping')
+	.setAuthor(coreconfig.botname, 'https://i.imgur.com/4AiJcoJ.png')
 	.setDescription('Pinging...')
 	.setTimestamp()
-  .setFooter('Creeper bot v1.0 by SpicyCombo#1665, made officially for Mikecraft Discord');
+  .setFooter(coreconfig.embedfooter);
 
 // Set the prefix
-let prefix = "c!";
+let prefix = config.prefix;
 // Now let's set the owner of the bot. So the eval command will work.
-let ownerID = "665691795685900316";
+let ownerID = config.ownerID;
 // Time to make it, so it will ignore all the messages from other bots. Including itself!
 
 client.on("message", (message) => {
   if (message.author.bot) return;
-  if (message.content.startsWith(prefix + "help")) { // If a message starts with the prefix then help, then it should do this operation
-    message.channel.send(helpEmbed) // It should send "helpEmbed". An operation that was defined above.
-  } else if (message.content.startsWith(prefix + "ping")) { // Now moving onto the ping command. Just like the help command. But we start with else if arguements. So the bot knows that we are moving onto the next command!
-    message.channel.send(pinglEmbed).then((msg) => { // First send the embed saying "Getting the ping"
-      ping = msg.createdTimestamp - message.createdTimestamp; // Here's where we define what the variable "ping" is.
-      const pingEmbed = new Discord.MessageEmbed() // And this is the embed that comes after the command the user typed.
-       .setFooter('Creeper bot v1.0 by SpicyCombo#1665, made officially for Mikecraft Discord')
-       .setAuthor('Creeper bot', 'https://i.imgur.com/4AiJcoJ.png')
-       .setColor(`#2fff00`)
-       .setTimestamp()
-       .setTitle(`Creeper bot - Ping`)
-       .setDescription(
-        ":ping_pong: | Pong! Bot's latency is `" + ping + 'ms`.'
-       );
-      message.channel.send(pingEmbed); // This is the step when the place when the bot sends the embed, that tells the user that the bot is getting the value of the embed.
-      msg.delete(); // Delete the previous message.
-    })
-  } else if (message.content.toLowerCase() == prefix + "restart") { // Note that this is an example and anyone can use this command.
-      message.channel.send(shutdownEmbed).then(() => { // And this is called "restart" because I have PM2 installed.
+    if (message.content.toLowerCase() == prefix + "shutdown") { // Note that this is an example and anyone can use this command.
+      message.channel.send(shutdownEmbed).then(() => {
           client.destroy(); // The place where the bot is shutdown
+          console.log(config.logstart); // Should also possibily tell us if anyone actually triggers this command.
       })
     } else if (message.content.startsWith(prefix + "lyrics")) { // Simple creeper command. If anyone types "creeper" or "Creeper" in the chat, then the bot will respond with "Aww man"
     message.channel.send(lyricsEmbed);
@@ -134,5 +148,4 @@ client.on("message", (message) => {
     }
 });
  
-client.login("get-your-token-from-discord"); //Set your bot's token here - P.S. I forgot to remove the bot's token, so now they reset the whole shit. Ahh
-
+client.login(config.token); //Set your bot's token here
