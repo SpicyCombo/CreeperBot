ae6fc61aa576026e300c4a57e07cddf536fa75fc
SpicyCombo <63688135+SpicyCombo@users.noreply.github.com>
2021-01-17 12:32:58 -0800

    configuration files, and package

---
+++ config.js
@@ -0,0 +1,24 @@
+const config = {
+    // Bot Owner's Discord User ID. I grabbed it from AnIdiotsGuide, and this is used for the eval command ;p
+    "ownerID": "665691795685900316",
+
+    // This is the message that should be displayed at console once the bot has started.
+    "logstart": "Creeper bot has started!",
+
+    // This is the message that should be displayed when the shutdown command is triggered
+    "logshutdown": "Creeper bot has now been shutdown.",
+
+    // Place where you put your really top secret token.
+    "token": "NzgwNTc0Nzg1MzIyMDkwNTM3.X7xE6w.YpKlnLJS08kGTkgxLVqlfhNjZJk",
+
+    // Prefix of the bot
+    "prefix": "c!",
+
+    // The footer text displayed at the end of every embed!
+    "embedfooter": "Made by SpicyCombo#1665",
+
+    // Name of the bot
+    "botname": "Creeper bot",
+};
+  
+  module.exports = config;
---
+++ core_config.js
@@ -0,0 +1,12 @@
+// This file is the file that makes the bot looks extreamly cool. 
+// Don't change unless you think your imaginations are cooler 😎
+
+const coreconfig = {
+    // Name of the bot
+    "botname": "Creeper bot",
+
+    // Footer of the bot
+    "embedfooter": "Made by SpicyCombo#1665"
+};
+  
+  module.exports = coreconfig;
